74022480001d19ffe6ef4bf01e0da81f5d2afd1e
Crockeo <crockeo@gmail.com>
2013-03-15 03:09:26 -0400

    Updated main's error function a wee bit.

--- src/Main.c
+++ src/Main.c
@@ -3,12 +3,15 @@
 #include "Interactive.h"
 #include "File.h"
 
+void err(const char* string) { fprintf(stderr, string); }
+
 int error(int value)
 {
 	// Printing out the error message(s)
 	switch (value)
 	{
-		case 1: printf("ERR CODE 1: Couldn't open source file."); break;
+		case 1: err("ERR CODE 1: Couldn't open source file."); break;
+		case 2: err("ERR CODE 2: Provided eval with a NULL string."); break;
 		default: break;
 	}
 
